9680fbc9d0a9603f6c4213439a48141e14e3a562
ag113ate <shota.terui@beach.ocn.ne.jp>
2018-10-08 02:03:15 +0000

    Add validation to InputReview model

--- app/models/input_review.rb
+++ app/models/input_review.rb
@@ -1,3 +1,12 @@
 class InputReview < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  
+  validates :menu_name, :comment, :total_score, {presence: true}
+  
+  validates :menu_name, length: {maximum: 100}
+  validates :comment, length: {maximum: 500}
+  
+  validates :total_score, \
+      numericality: {greater_than_or_equal_to: 0.5, less_than_or_equal_to: 5.0}
+      
 end
